1d93bb34011ac7049ed806769d0d37e00a0e25c1
Guillaume Fraux <guillaume.fraux@ens.fr>
2015-06-01 19:41:39 +0200

    Use the right shared lib extension

--- deps/build.jl
+++ deps/build.jl
@@ -13,13 +13,20 @@ prefix = joinpath(BinDeps.depsdir(libchemharp), "usr")
 srcdir = joinpath(BinDeps.depsdir(libchemharp), "src", "Chemharp-$version")
 builddir = joinpath(BinDeps.depsdir(libchemharp), "builds", "Chemharp-$version")
 
+if VERSION >= v"0.4.0-dev"
+    dlext = Libdl.dlext
+else
+    dlext = Base.Sys.shlib_ext
+end
+
+
 provides(BuildProcess,
     (@build_steps begin
         GetSources(libchemharp)
         CreateDirectory(builddir)
         @build_steps begin
             ChangeDirectory(builddir)
-            FileRule(joinpath(prefix, "lib", "libchemharp.dylib"),
+            FileRule(joinpath(prefix, "lib", "libchemharp.$(dlext)"),
                 @build_steps begin
                     `cmake -DCMAKE_INSTALL_PREFIX="$prefix" $srcdir`
                     `make`
