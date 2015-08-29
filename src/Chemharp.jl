7641e6088b6e90a4705e572f6c989809a6b5792f
Guillaume Fraux <guillaume.fraux@ens.fr>
2015-08-29 22:53:57 +0200

    Disable static compilation on Windows

--- src/Chemharp.jl
+++ src/Chemharp.jl
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-VERSION >= v"0.4.0-dev+6521" && __precompile__()
+@unix_only VERSION >= v"0.4.0-dev+6521" && __precompile__()
 
 module Chemharp
     module lib
