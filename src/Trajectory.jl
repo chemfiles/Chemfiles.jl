aaefbd2f2460c7d5ae74e3d62550f9212363e40f
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-07-13 12:14:38 +0200

    Check that the trajectory is open when working with it

--- src/Trajectory.jl
+++ src/Trajectory.jl
@@ -16,6 +16,7 @@ end
 free(trajectory::Trajectory) = close(trajectory)
 
 function Base.read!(trajectory::Trajectory, frame::Frame)
+    assert(isopen(trajectory))
     check(
         lib.chfl_trajectory_read(trajectory.handle, frame.handle)
     )
@@ -23,11 +24,13 @@ function Base.read!(trajectory::Trajectory, frame::Frame)
 end
 
 function Base.read(trajectory::Trajectory)
+    assert(isopen(trajectory))
     frame = Frame()
     return read!(trajectory, frame)
 end
 
 function read_step!(trajectory::Trajectory, step::Integer, frame::Frame)
+    assert(isopen(trajectory))
     check(
         lib.chfl_trajectory_read_step(trajectory.handle, UInt64(step), frame.handle)
     )
@@ -35,11 +38,13 @@ function read_step!(trajectory::Trajectory, step::Integer, frame::Frame)
 end
 
 function read_step(trajectory::Trajectory, step::Integer)
+    assert(isopen(trajectory))
     frame = Frame()
     return read_step!(trajectory, step, frame)
 end
 
 function Base.write(trajectory::Trajectory, frame::Frame)
+    assert(isopen(trajectory))
     check(
         lib.chfl_trajectory_write(trajectory.handle, frame.handle)
     )
@@ -47,6 +52,7 @@ function Base.write(trajectory::Trajectory, frame::Frame)
 end
 
 function set_topology!(trajectory::Trajectory, topology::Topology)
+    assert(isopen(trajectory))
     check(
         lib.chfl_trajectory_set_topology(trajectory.handle, topology.handle)
     )
@@ -54,6 +60,7 @@ function set_topology!(trajectory::Trajectory, topology::Topology)
 end
 
 function set_topology!(trajectory::Trajectory, filename::AbstractString, format::AbstractString = "")
+    assert(isopen(trajectory))
     check(
         lib.chfl_trajectory_topology_file(trajectory.handle, pointer(filename), pointer(format))
     )
@@ -61,6 +68,7 @@ function set_topology!(trajectory::Trajectory, filename::AbstractString, format:
 end
 
 function set_cell!(trajectory::Trajectory, cell::UnitCell)
+    assert(isopen(trajectory))
     check(
         lib.chfl_trajectory_set_cell(trajectory.handle, cell.handle)
     )
@@ -68,6 +76,7 @@ function set_cell!(trajectory::Trajectory, cell::UnitCell)
 end
 
 function nsteps(trajectory::Trajectory)
+    assert(isopen(trajectory))
     result = Ref{UInt64}(0)
     check(
         lib.chfl_trajectory_nsteps(trajectory.handle, result)
