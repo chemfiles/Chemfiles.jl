62ace922e3ec0a3c5c06ec1e47f7ea88021dc31f
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-07-13 13:21:30 +0200

    Do not export ChemfilesWarning

--- src/errors.jl
+++ src/errors.jl
@@ -3,14 +3,13 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+export ChemfilesError
 
 type ChemfilesError <: Exception
     message::AbstractString
 end
 
 Base.show(io::IO, e::ChemfilesError) = show(io, "Chemfiles error: $(e.message)")
-
-export ChemfilesError, ChemfilesWarning
 
 function check(result::Integer, message = "Unknown error")
     if result != 0
