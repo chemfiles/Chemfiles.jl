36ef6a3949c9e416670dfe8705fe7da035b4c622
sunoru <s@sunoru.com>
2019-02-08 18:20:03 -0500

    Fix typos in the docstring.

--- src/Chemfiles.jl
+++ src/Chemfiles.jl
@@ -31,7 +31,7 @@ module Chemfiles
 
     """
     A ``Trajectory`` represents a simulation file on the hard drive. It can read
-    or write one or many ``Frame`` to this file. The file format can be
+    or write one or many ``Frame``s to this file. The file format can be
     automatically determined from the extention, or manually specified.
     Writing to a ``Trajectory`` is buffered, which means that one needs to
     ``close()`` the trajectory and flush the buffer before being able to read the
@@ -49,8 +49,8 @@ module Chemfiles
 
     """
     A ``Topology`` describes the organisation of the particles in the system:
-    what are there names, how are they bonded together, *etc.* A ``Topology``
-    is a list of ``Atom`` in the system, together with the list of bonds between
+    what their names are, how they are bonded together, *etc.* A ``Topology``
+    is a list of ``Atom``s in the system, together with the list of bonds between
     the atoms.
     """
     mutable struct Topology
@@ -73,7 +73,7 @@ module Chemfiles
       - atom charge
 
     The atom name is usually an unique identifier ("H1", "C_a") while the atom
-    type will be shared between all particles of the same type: "H", "Ow", "CH3".
+    type will be shared among all particles of the same type: "H", "Ow", "CH3".
     """
     mutable struct Atom
         handle :: Ptr{lib.CHFL_ATOM}
@@ -86,8 +86,8 @@ module Chemfiles
     end
 
     """
-    An ``UnitCell`` describe the bounding box of a system. It is represented by
-    three base vectors of lengthes ``a``, ``b`` and ``c``; and the angles
+    A ``UnitCell`` describes the bounding box of a system. It is represented by
+    three base vectors of lengths ``a``, ``b`` and ``c``; and the angles
     between these vectors are ``alpha``, ``beta`` and ``gamma``.
     """
     mutable struct UnitCell
@@ -102,8 +102,8 @@ module Chemfiles
 
     """
     A ``Frame`` holds data for one step of a simulation. As not all formats
-    provides all the types of informations, some fields may be initialized to a
-    default value. A ``Frame`` may contains the following data:
+    provide all the types of information, some fields may be initialized to a
+    default value. A ``Frame`` may contain the following data:
 
     - Positions for all the atoms in the system;
     - Velocities for all the atoms in the system;
@@ -121,7 +121,7 @@ module Chemfiles
     end
 
     """
-    A ``Selection`` allow to select a group of atoms. Examples of selections are
+    A ``Selection`` is used to select a group of atoms. Examples of selections are
     "name H" and "(x < 45 and name O) or name C". See the `full documentation
     <http://chemfiles.org/chemfiles/latest/selections.html>`_ for more
     information about the selection language.
