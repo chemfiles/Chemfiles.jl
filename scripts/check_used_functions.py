930eda8c58376f6f3642b9533139e6dfc0601081
ezavod <ezavod@gmx.de>
2020-06-15 16:45:11 +0200

    refactor(scripts): consistent formatting and naming in check funcs

--- scripts/check_used_functions.py
+++ scripts/check_used_functions.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 """
-Check that all the functions defined in the C API are being used.
+Check that all the functions defined in the C API are
+effectively used in the chemfiles binding.
 """
 import os
 import sys
 
 IGNORED = ["chfl_trajectory_open"]
 ERROR = False
-ROOT = os.path.dirname(os.path.dirname(os.path.abspath((__file__))))
+ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+
 
 def error(message):
     print(message)
     global ERROR
     ERROR = True
 
-def all_functions():
+
+def functions_list():
     functions = []
     with open(os.path.join(ROOT, "src", "generated", "cdef.jl")) as fd:
         for line in fd:
@@ -24,10 +27,11 @@ def all_functions():
                 functions.append(name)
     return functions
 
+
 def read_all_source():
     source = ""
-    for (dirpath, _, pathes) in os.walk(os.path.join(ROOT, "src")):
-        for path in pathes:
+    for (dirpath, _, paths) in os.walk(os.path.join(ROOT, "src")):
+        for path in paths:
             if path != "cdef.jl" and path.endswith(".jl"):
                 with open(os.path.join(ROOT, dirpath, path)) as fd:
                     source += fd.read()
@@ -41,8 +45,9 @@ def check_functions(functions, source):
 
 
 if __name__ == '__main__':
-    functions = all_functions()
+    functions = functions_list()
     source = read_all_source()
     check_functions(functions, source)
+
     if ERROR:
         sys.exit(1)
