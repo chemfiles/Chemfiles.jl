a0ad56339512194ecb13e2a63916a193901f73aa
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2020-06-16 17:44:24 +0200

    Update CI to build the new docs

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -1,25 +1,20 @@
 #!/bin/bash -xe
 
-# Install doc dependencies
 cd $TRAVIS_BUILD_DIR
-pip install --upgrade --user -r doc/requirements.txt
+
+# Install doc dependencies
+julia --check-bounds=yes --inline=no -e 'using Pkg; Pkg.add("Documenter");'
 
 # Get previous documentation
 git clone https://github.com/chemfiles/Chemfiles.jl --branch gh-pages gh-pages
 rm -rf gh-pages/.git
 
 # Build documentation
-cd doc
-make html
-rm -rf _build/html/.doctrees/ _build/html/.buildinfo
-rm -rf _build/html/_static/bootswatch-2.3.2/ _build/html/_static/bootstrap-2.3.2/
-shopt -s extglob
-cd _build/html/_static/bootswatch-* && rm -rf !(flatly) && cd -
-cd ..
+julia docs/make.jl
 
 if [[ "$TRAVIS_TAG" != "" ]]; then
-    mv doc/_build/html/ gh-pages/$TRAVIS_TAG
+    mv docs/build/ gh-pages/$TRAVIS_TAG
 else
     rm -rf gh-pages/latest
-    mv doc/_build/html/ gh-pages/latest
+    mv docs/build/ gh-pages/latest
 fi
